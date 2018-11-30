c0cbc663489edccce480b8784e31248bba0de536
Isabel Navarro-Laurent <isabelnavarro-laurent@Isabels-MacBook-Air.local>
2018-11-30 02:43:20 -0500

    Initial commit

---
+++ src/main/java/springboot403/demo/Springboot403Application.java
@@ -0,0 +1,12 @@
+package springboot403.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot403Application.class, args);
+    }
+}
